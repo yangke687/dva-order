94b65107891bebe2a9e8bb475ad3c612ab6a3722
Ke Yang <yangke687@gmail.com>
2019-06-11 15:32:16 +0800

    create Login / Register template page

--- src/pages/user/login.js
+++ src/pages/user/login.js
@@ -1,7 +1,27 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Form, Input, Button } from 'antd';
+import Icon from 'Assets/icon.png';
+import styles from './index.scss';
 
 export default class login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className={styles.account}>
+        <img src={Icon} alt="" className={styles.logo} />
+        <Form className="account-form">
+          <Form.Item label="邮箱">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码">
+            <Input type="password" />
+          </Form.Item>
+          <Form.Item>
+            <Button className="btn" type="primary">
+              注册
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
   }
 }
--- src/pages/user/reg.js
+++ src/pages/user/reg.js
@@ -1,7 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Form, Input, Button } from 'antd';
+import Icon from 'Assets/icon.png';
+import styles from './index.scss';
 
 export default class reg extends Component {
   render() {
-    return <div>Reg</div>;
+    return (
+      <div className={styles.account}>
+        <img src={Icon} alt="" className={styles.logo} />
+        <Form className="account-form">
+          <Form.Item label="邮箱">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码">
+            <Input type="password" />
+          </Form.Item>
+          <Form.Item label="确认密码">
+            <Input type="password" />
+          </Form.Item>
+          <Form.Item>
+            <Button className="btn" type="primary">
+              注册
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
   }
 }
